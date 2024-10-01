de86a304f852849fb927572537e1b6ca0014f422
mariaeugeniacecchini <cecchinitest@gmail.com>
2024-09-30 21:46:51 -0300

    primer commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#Menu opciones
+print ("Menu gestion de productos")
+print ("1. Alta de productos nuevos")
+print ("2. Consulta de datos de productos")
+print ("3. Modificar la cantidad en stock de productos")
+print ("4. Dar de baja en productos")
+print ("5. Listado completo de productos")
+print ("6. Lista de productos con cantidad bajo minimo")
+print ("7. Salir")
+
+opcion = int (input ("Por favor, selecciona una opcion (1-7): "))
+print ("Has ingresado" , opcion)
